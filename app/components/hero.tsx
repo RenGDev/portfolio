864aa9b9ba73219176b50bf84705787eaea6994d
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2026-08-26 19:47:48 -0300

    Feat: Update hero section with enhanced title styling and improved layout

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Hero() {
     return (
         <div id="hero" className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold italic">LORENZO GONÇALVES</h1>
-            <h1 className="text-4xl font-bold italic"> ‹ <span className="text-primary">FULLSTACK</span> DEVELOPER ›</h1>
+            <h1 className="text-6xl font-bold italic max-w-fit typing_entry">LORENZO GONÇALVES</h1>
+            <h1 className="text-4xl font-bold italic typing_words"></h1>
         </div>
     )
 }
