69b91694478ad2482dd9b582de129c4140363462
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2026-09-13 23:02:53 -0300

    Feat: Update header links and enhance hero title styling

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
         <div id="hero" className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-6xl font-bold italic max-w-fit typing_entry">LORENZO GONÇALVES</h1>
+            <h1 className="text-6xl font-bold italic max-w-fit typing_entry text-shadow-md text-shadow-primary">LORENZO GONÇALVES</h1>
             <h1 className="text-4xl font-bold italic typing_words"></h1>
         </div>
     )
