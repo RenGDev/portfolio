a9e080ac61500ae13304fdffb2fe7c9ff668df73
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2026-09-15 17:37:45 -0300

    Feat: Implement responsive mobile menu component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { motion, useMotionValueEvent, useScroll } from "motion/react";
 import { useState } from "react";
+import MenuMobile from "./ui/menuMobile";
 
 export default function Header(){
 
@@ -23,7 +24,7 @@ export default function Header(){
 
     return(
         <motion.div 
-            className="flex items-center justify-between px-9 text-white sticky top-0 z-50 h-20"
+            className="flex items-center md:justify-between px-9 text-white sticky top-0 z-50 h-20"
             animate={{ y: hidden ? "-100%" : "0%" }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
           
@@ -32,17 +33,19 @@ export default function Header(){
                 src="/logo_vermelha.png"
                 width={100}
                 height={100}
-                alt="Logo"        
+                alt="Logo"
+                className="m-auto md:m-0"        
             />
             <nav>
-                <ul className="flex space-x-9">
+                <ul className="hidden md:flex space-x-9">
                     <li className="hover:text-secondary duration-100 text-sm font-bold relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-primary after:ease-in after:duration-200 hover:after:w-full"><a href="#hero">HERO</a></li>
                     <li className="hover:text-secondary duration-100 text-sm font-bold relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-primary after:ease-in after:duration-200 hover:after:w-full"><a href="#about">ABOUT</a></li>
                     <li className="hover:text-secondary duration-100 text-sm font-bold relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-primary after:ease-in after:duration-200 hover:after:w-full"><a href="#projects">PROJECTS</a></li>
-                    <li className="hover:text-secondary duration-100 text-sm font-bold relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-primary after:ease-in after:duration-200 hover:after:w-full"><a href="#contact">CONTACT</a></li>
-                    
+                    <li className="hover:text-secondary duration-100 text-sm font-bold relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-primary after:ease-in after:duration-200 hover:after:w-full"><a href="#contact">CONTACT</a></li>       
                 </ul> 
             </nav>
+
+            <MenuMobile />
         </motion.div>
     )
 }
