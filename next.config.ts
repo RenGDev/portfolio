2ee88bdf64edc6f1d3a676575eb19287d0c4221f
Lorenzo Gonçalves <lorenzodequadrosgoncalves@gmail.com>
2026-09-22 01:26:53 -0300

    chore: update dependencies and devDependencies in package.json
    feat: add seed command to prisma7.config.ts
    
    fix: correct tsconfig.json to include prisma.ts in the lib

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-91ce1a23bc1d43b283b5f1832668a531.r2.dev',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/manager',
+        destination: '/manager/projects',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
